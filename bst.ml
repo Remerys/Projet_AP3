ce05f27a506cee19778ba2256f8c37a4b5e31e8b
Lilian <liliannalil347@gmail.com>
2023-11-08 19:08:02 +0100

    yes

--- bst.ml
+++ bst.ml
@@ -1,1 +1,32 @@
+#load "btreeS.cmo";;
+#load "useBtree.cmo";;
+open BtreeS;;
+open UseBtree;;
+
+type 'a t_bst = 'a btreeS;; 
+
+let rec bst_linsert(bst, e : 'a t_bst * 'a) : 'a t_bst =
+  if (btreeS_isempty(bst))
+  then btreeS_rooting(e, btreeS_empty(), btreeS_empty())
+  else
+    let r : 'a = btreeS_root(bst) in
+    if (e < r)
+    then btreeS_rooting(r , bst_linsert(btreeS_subleft(bst), e), btreeS_subright(bst))
+    else btreeS_rooting(r , btreeS_subleft(bst), bst_linsert(btreeS_subright(bst), e))
+;;
+
+let bst_lbuild(l : 'a list) : 'a t_bst =
+  bst_lbuild_aux(l, btreeS_empty())
+;;
+
+
+let rec bst_lbuild_aux(l, t : 'a list * 'a t_bst) : 'a t_bst =
+  if (l = [])
+  then t
+  else bst_lbuild(List.tl(l), bst_linsert(t, List.hd(l))
+;;
+
+let leaf : int t_bst = bst_lbuild([1;2;4;8;16]);;
+btree_to_string(leaf);;
+
 
--- useBtree.ml
+++ useBtree.ml
@@ -1,7 +1,4 @@
-#load "btreeS.cmo";;
-
 open BtreeS;;
-
 let rec size(bt_tree : 'a btreeS) : int =
   if(btreeS_isempty(bt_tree))
   then 0
@@ -14,35 +11,15 @@ let max(a, b : int * int) : int =
   else b
 ;;
 
-
 let rec height(bt_tree : 'a btreeS) : int =
   if (btreeS_isempty(bt_tree))
   then -1 (*not defined*)
   else 1 + max( height(btreeS_subleft(bt_tree)), height(btreeS_subright(bt_tree)))
 ;;
 
-let rec btree_to_string(bt_tree : string btreeS) : string =
-  "(" ^ btreeS_root(bt_tree) ^ "," ^ btree_to_string(btreeS_subleft(bt_tree)) ^ "," ^ btree_to_string(btree_subright(bt_tree)) ^ ")"
+let rec btree_to_string(bt_tree : int btreeS) : string =
+  if btreeS_isempty(bt_tree)
+  then "empty"
+  else
+  "(" ^ string_of_int(btreeS_root(bt_tree)) ^ "," ^ btree_to_string(btreeS_subleft(bt_tree)) ^ "," ^ btree_to_string(btreeS_subright(bt_tree)) ^ ")"
 ;;
-
-
-let tEmpty : int btreeS = btreeS_empty();;
-
-let tLeaf : int btreeS = btreeS_rooting(1, tEmpty, tEmpty);;
-
-let tTree : int btreeS = btreeS_rooting(2, tLeaf, tEmpty);;
-
-let tTree2 : int btreeS = btreeS_rooting(3, tTree, tLeaf);;
-
-size(tEmpty);;
-size(tLeaf);;
-size(tTree);;
-size(tTree2);;
-
-height(tEmpty);;
-height(tLeaf);;
-height(tTree);;
-height(tTree2);;
-
-btree_to_string(tLeaf);;
-
